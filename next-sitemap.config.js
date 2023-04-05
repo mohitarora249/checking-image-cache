835e735e2473068026e0f683cefd62b9594e8490
mohitarora249 <mohitarora249@gmail.com>
2023-04-05 21:45:03 +0530

    init

---
+++ next-sitemap.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteUrl: "https://checking-image-cache.vercel.app/",
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+};
